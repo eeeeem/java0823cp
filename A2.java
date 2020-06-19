53726e8cb4e8c59559914fae38299f00cfa5f83b
eeeeem <66980856+eeeeem@users.noreply.github.com>
2020-06-19 09:59:52 +0800

    Add files via upload

---
+++ A2.java
@@ -0,0 +1,13 @@
+ class Student{
+            final String name="小明";
+            public void introduce(){
+         System.out.println("我是一名学生，我叫"+name);
+}
+}
+            public class A2{
+         public static void main(String[] args)
+ {
+             Student k=new Student();
+            k.introduce();
+}
+}
