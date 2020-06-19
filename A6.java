040b21e9e426ea3b82aee2f872150d1c983b23a7
eeeeem <66980856+eeeeem@users.noreply.github.com>
2020-06-19 10:03:11 +0800

    Add files via upload

---
+++ A6.java
@@ -0,0 +1,14 @@
+public class A6{
+            public static void main (String args[]){
+                     try{ int result=divide(4, 0);
+System.out.println (result);}
+catch(Exception e){
+System.out.println("“Ï≥£Ã· æ£∫"+e. getMessage());
+}
+}
+public static int divide(int x, int y){
+int result=x/y;
+return result;
+
+}
+}
